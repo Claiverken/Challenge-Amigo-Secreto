26ce51836999475701334053ab7232569eddd313
Claiver Sampaio <claiverkenpachi98@gmail.com>
2025-01-20 02:33:47 -0300

    Add Name List

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+  let nomeInput = document.querySelector("#amigo");
+  let nome = nomeInput.value.trim();
+  if (nome === "") {
+    alert("Por favor, insira um nome.");
+  } else {
+    amigos.push(nome);
+    nomeInput.value = "";
+
+    atualizarLista();
+  }
+}
+
+
+function atualizarLista() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++){
+    let amigo = amigos[i];
+
+    let item = document.createElement("li");
+    item.textContent = amigo;
+
+    lista.appendChild(item);
+  }
+}
