fb5bb3024ec3b0d8f526f407a68df4db91fd79c7
Claiver Sampaio <claiverkenpachi98@gmail.com>
2025-01-20 15:19:02 -0300

    add func sortearAmigos

--- app.js
+++ app.js
@@ -14,17 +14,27 @@ function adicionarAmigo() {
   }
 }
 
-
 function atualizarLista() {
   let lista = document.getElementById("listaAmigos");
   lista.innerHTML = "";
 
-  for (let i = 0; i < amigos.length; i++){
+  for (let i = 0; i < amigos.length; i++) {
     let amigo = amigos[i];
 
     let item = document.createElement("li");
     item.textContent = amigo;
 
     lista.appendChild(item);
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("Sem nomes para sortear. Por favor, insira os nomes.");
+  } else {
+    let nomeSorteado = Math.floor(Math.random() * amigos.length);
+    let resultado = document.querySelector("#resultado");
+    resultado.innerHTML = "";
+    resultado.textContent = "O seu amigo secreto é: " + amigos[nomeSorteado];
   }
 }
